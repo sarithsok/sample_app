ec31588400812788d6ac6ca476255c2ff836192e
Sarith Sok <sarithstyle@yahoo.com>
2012-02-23 11:17:18 +0700

    Finished exercises of chapter 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -40,6 +40,26 @@ describe UsersController do
       get :new
       response.should be_success
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do 
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
   end
 
   it "should have the right title" do
